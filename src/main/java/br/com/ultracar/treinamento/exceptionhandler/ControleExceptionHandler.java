25d1169e8b43a8d01093a2ade6f6f4973c7ea625
CassiaRibeiro <crra295@gmail.com>
2019-07-17 21:00:40 -0300

    Commit

--- src/main/java/br/com/ultracar/treinamento/exceptionhandler/ControleExceptionHandler.java
+++ src/main/java/br/com/ultracar/treinamento/exceptionhandler/ControleExceptionHandler.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice   //Com essa anotação a classe ira 'observar' toda a aplicacao 
+@ControllerAdvice   //Com essa anotação a classe ira 'observar' toda a aplicacao para retornar o erro
 public class ControleExceptionHandler extends ResponseEntityExceptionHandler {
 
 
